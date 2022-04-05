95f79b9a6e538f3bfa4c8df9ef3b587639f47839
Parskie00 <par.marcjoseph.l.5400@gmail.com>
2022-04-05 10:59:55 +0000

    commit

--- laravel/resources/views/cert.blade.php
+++ laravel/resources/views/cert.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=1, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="app.css">
 </head>
 <body>
 <div class="topbar">
