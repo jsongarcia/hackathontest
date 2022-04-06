ab2734c6ce02ee0bc2fecbd57c100c273cd99985
jsongarcia <charlesjason.garcia.m@bulsu.edu.ph>
2022-04-06 10:10:32 +0000

    a

--- laravel/resources/views/educationHome.blade.php
+++ laravel/resources/views/educationHome.blade.php
@@ -4,6 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=1, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 <div class="topbar">
